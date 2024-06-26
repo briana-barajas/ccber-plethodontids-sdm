51d4c7451162ec2efeccc8f2d730146bd042085d
bbarajas429 <bricavii2012@gmail.com>
2024-06-26 01:05:07 +0000

    Maxent only test run, final script

--- final-model-runs/run-models.R
+++ final-model-runs/run-models.R
@@ -197,11 +197,11 @@ point_dir <- here("data", "CampRoberts_spatial_data")
 maxent_all_predictions <- data.frame()
 
 # ................run all & reduced Maxent............
-for (i in seq(1:8)) {
+for (i in seq(1:2)) {
   maxent_res <- tune_maxent(plot_number = i,
-                      point_dir = point_dir,
-                      rast_dir = rast_dir,
-                      include_variables = "both")
+                            point_dir = point_dir,
+                            rast_dir = rast_dir,
+                            include_variables = "both")
   
   # ...............store results as vars................
   maxent_p_coords <- maxent_res[["p_coords"]]
@@ -218,26 +218,115 @@ for (i in seq(1:8)) {
   maxent_id_reduced <- which.max(maxent_gs_reduced@results$test_AUC)
   maxent_auc_reduced <- max(maxent_gs_reduced@results$test_AUC)
   maxent_best_mod_reduced <- combineCV(maxent_gs_reduced@models[[maxent_id_reduced]])
-
+  
   
   # .................generate predictions...............
-  maxent_map_all <- predict(maxent_best_mod_all, data = maxent_pred_stack)
-  maxent_map_reduced <- predict(maxent_best_mod_reduced, data = maxent_pred_stack)
-  # ....................................................
+  maxent_map_all <- predict(maxent_best_mod_all, data = maxent_pred_stack,
+                            type = "cloglog", clamp = TRUE)
   
+  maxent_map_reduced <- predict(maxent_best_mod_reduced, data = maxent_pred_stack,
+                                type = "cloglog", clamp = TRUE)
   
+  rm(maxent_res, maxent_id_all, maxent_id_reduced, maxent_best_mod_reduced, maxent_best_mod_all)
   
+  # ...............isolate all predictions..............
+  pres_prediction_all <- extract(maxent_map_all, maxent_p_coords, xy = TRUE, ID = FALSE) %>% 
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 1,
+           variables = "all")
   
+  abs_prediction_all <- extract(maxent_map_all, maxent_bg_coords, xy = TRUE, ID = FALSE) %>%
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 0,
+           variables = "all")
+  
+  # .............isolate reduced predictions............
+  pres_prediction_reduced <- extract(maxent_map_reduced, maxent_p_coords, xy = TRUE, ID = FALSE) %>%
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 1,
+           variables = "reduced")
+  
+  abs_prediction_reduced <- extract(maxent_map_reduced, maxent_bg_coords, xy = TRUE, ID = FALSE) %>%
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 0,
+           variables = "reduced")
+  
+  rm(maxent_p_coords, maxent_bg_coords, maxent_map_reduced, maxent_map_all)
+  
+  # ....................combine results.................
+  combined_predictions <- rbind(pres_prediction_all, abs_prediction_all,
+                                pres_prediction_reduced, abs_prediction_reduced) %>% 
+    mutate(plot = i,
+           test_auc = ifelse(variables == "all", maxent_auc_all, maxent_auc_reduced))
+  
+  maxent_all_predictions <- rbind(maxent_all_predictions, combined_predictions)
+  
+  rm(combined_predictions, pres_prediction_all, abs_prediction_all, pres_prediction_reduced, abs_prediction_reduced)
   
   
 } # END Maxent all & reduced loop
 
 
 
+## ===================================================================
+##                    Maxent Modeling - Select Variables          ----
+## ===================================================================
 
+# ......................preparation...................
 
+# set data directory
+rast_dir <- here("data", "raster-stacks", "full-extent", "select-vars")
+point_dir <- here("data", "CampRoberts_spatial_data")
 
+# ................Run model w/ select variables.......
+for (i in seq(1:8)) {
+  
+  maxent_res <- tune_maxent(plot_number = i,
+                            point_dir = point_dir,
+                            rast_dir = rast_dir,
+                            include_variables = "all")
+  
+  # ...............store results as vars................
+  maxent_p_coords <- maxent_res[["p_coords"]]
+  maxent_bg_coords <- maxent_res[["bg_coords"]]
+  maxent_pred_stack <- maxent_res[["maxent_pred_stack"]]
+  maxent_gs_all <- maxent_res[["maxent_gs_all"]]
+  
+  # .................select best models.................
+  maxent_id_all <- which.max(maxent_gs_all@results$test_AUC)
+  maxent_auc_all <- max(maxent_gs_all@results$test_AUC)
+  maxent_best_mod_all <- combineCV(maxent_gs_all@models[[maxent_id_all]])
+  
+  # .................generate predictions...............
+  maxent_map_all <- predict(maxent_best_mod_all, data = maxent_pred_stack,
+                            type = "cloglog", clamp = TRUE)
+  
+  # ...............isolate all predictions..............
+  pres_prediction_all <- extract(maxent_map_all, maxent_p_coords, xy = TRUE, ID = FALSE) %>% 
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 1,
+           variables = "select")
+  
+  abs_prediction_all <- extract(maxent_map_all, maxent_bg_coords, xy = TRUE, ID = FALSE) %>%
+    rename(prediction = lyr1) %>%
+    mutate(pres_abs = 0,
+           variables = "select")
+  
+  rm(maxent_p_coords, maxent_bg_coords, maxent_map_all)
+  
+  # ....................combine results.................
+  combined_predictions <- rbind(pres_prediction_all, abs_prediction_all) %>%
+    mutate(plot = i,
+           test_auc = maxent_auc_all)
+  
+  maxent_all_predictions <- rbind(maxent_all_predictions, combined_predictions)
+  
+  rm(combined_predictions, pres_prediction_all, abs_prediction_all, maxent_gs_all, maxent_gs_reduced, maxent_pred_stack)
 
+} # END Maxent select variable loop
+
+# ..................write CSV results ................
+write_csv(maxent_all_predictions, here(output_dir, "maxent_results.csv"))
 
 
 
