e24b55d62a552f68311ff48203e4d3a85d8183c4
bbarajas429 <140910852+bbarajas429@users.noreply.github.com>
2024-08-20 13:15:00 -0700

    Updating plots

--- 4_result_figures.R
+++ 4_result_figures.R
@@ -4,7 +4,7 @@
 
 library(tidyverse)
 library(here)
-library(tmap)
+library(viridis)
 
 maxent_results <- read_csv(here("results", "maxent_results.csv"))
 brt_p_results <- read_csv(here("results", "brt_p_results.csv"))
@@ -13,41 +13,195 @@ brt_pa_results <- read_csv(here("results", "brt_pa_results.csv"))
 
 
 ## ===================================================================
-##              Calculate Correlation & Combine Results           ----
+##                  Calculate Aggregate Correlation               ----
 ## ===================================================================
+
+## ...................calculate Maxent cor....................
 
 # presence-only methods
-maxent_results <- maxent_results %>% 
+maxent_cor <- maxent_results %>% 
   group_by(variables) %>% 
   summarise(cor = cor(prediction, pres_abs),
             auc = mean(test_auc),
-            method = "maxent")
+            Method = "Maxent")
 
-brt_p_results <- brt_p_results %>% 
+## ................calculate BRT pres-only cor................
+brt_p_cor <- brt_p_results %>% 
   group_by(variables) %>% 
   summarise(cor = cor(prediction, pres_abs),
             auc = mean(test_auc),
-            method = "brt_p")
+            Method = "BRT")
 
-# full results
-results <- brt_pa_results %>% 
+## ...................create full corr df.....................
+# calculate cor for BRT pres-abs
+results_cor <- brt_pa_results %>% 
   group_by(variables) %>% 
   summarise(cor = cor(prediction, pres_abs),
             auc = mean(test_auc),
-            method = "brt_pa") %>% 
-  rbind(maxent_results, brt_p_results)
+            Method = "BRT PA") %>% 
+  
+  # bind to pres-only correlations
+  rbind(maxent_cor, brt_p_cor) %>% 
+  rename(Variables = variables)
 
-rm(brt_pa_results, brt_p_results, maxent_results)
+rm(maxent_cor, brt_p_cor)
+
+## ===================================================================
+##                 Calculate Plot Correlations                    ----
+## ===================================================================
+
+## ...................calculate Maxent cor....................
+# presence-only methods
+maxent_cor <- maxent_results %>% 
+  group_by(variables, plot) %>% 
+  summarise(cor = cor(prediction, pres_abs),
+            auc = mean(test_auc),
+            Method = "Maxent")
+
+## ................calculate BRT pres-only cor................
+brt_p_cor <- brt_p_results %>% 
+  group_by(variables, plot) %>% 
+  summarise(cor = cor(prediction, pres_abs),
+            auc = mean(test_auc),
+            Method = "BRT")
+
+## ...................create full corr df.....................
+# calculate cor for BRT pres-abs
+results_plot_cor <- brt_pa_results %>% 
+  group_by(variables, plot) %>% 
+  summarise(cor = cor(prediction, pres_abs),
+            auc = mean(test_auc),
+            Method = "BRT PA") %>% 
+  
+  # bind to pres-only correlations
+  rbind(maxent_cor, brt_p_cor) %>% 
+  rename(Variables = variables)
+
+rm(maxent_cor, brt_p_cor)
 
 
 ## ===================================================================
-##                             Plots                              ----
+##                        Figures - Aggregate Cor                 ----
 ## ===================================================================
-ggplot(results, aes(x = auc,
-                    y = cor, 
-                    shape = method,
-                    col = variables)) +
-  geom_point(size = 3) +
-  scale_color_manual(values = c("maroon", "dodgerblue", "yellowgreen")) +
+## ..............correlation plot - presence only.............
+results_cor %>% 
+  filter(Method != "BRT PA") %>% 
+  
+  ggplot() +
+  geom_point(aes(x = auc,
+                 y = cor,
+                 # col = Method, 
+                 shape = Variables),
+             size = 3.5) +
+  
+  # specify color and shape for consistency
+  # scale_color_manual(values = c("Maxent" = "#009988",
+  #                               "BRT" = "#EE7733")) +
+  scale_shape_manual(values = c("all" = 16,
+                                "reduced" = 17,
+                                "select" = 15)) +
+  
+  # add model labels
+  annotate("text", 
+           x = 0.68, 
+           y = 0.32, 
+           label = "Maxent",
+           size = 5) +
+  
+  annotate("text",
+           x = 0.63,
+           y = 0.21,
+           label = "BRT",
+           size = 5) +
+  
+  # plot labels & theme
+  guides(col = FALSE) +
+  theme_bw()+ 
+  labs(x = "Test AUC", y = "Correlation")
+
+
+## ................correlation plot - all models..............
+ggplot(results_cor, aes(x = auc,
+                        y = cor,
+                        # col = Method, 
+                        shape = Variables)) +
+  geom_point(size = 3.5) +
+  
+  # specify color and shape for consistency
+  # scale_color_manual(values = c("Maxent" = "#009988",
+  #                               "BRT" = "#EE7733",
+  #                               "BRT PA" = "#EE3377")) +
+  scale_shape_manual(values = c("all" = 16,
+                                "reduced" = 17,
+                                "select" = 15)) +
+  
+  # add model labels
+  annotate("text", 
+           x = 0.68, 
+           y = 0.32, 
+           label = "Maxent",
+           size = 4) +
+  
+  annotate("text",
+           x = 0.63,
+           y = 0.21,
+           label = "BRT",
+           size = 4) +
+  
+  annotate("text",
+           x = 0.8,
+           y = 0.65,
+           label = "BRT PA",
+           size = 4) +
+  
+  # plot labels & theme 
   labs(x = "Test AUC", y = "Correlation") +
-  theme_minimal()
+  guides(col = FALSE) +
+  theme_bw()
+
+## ===================================================================
+##                         Figures - Sample Size                  ----
+## ===================================================================
+# .................. add number of occurrence points.................
+results_plot_cor <- results_plot_cor %>% 
+  mutate(no_p_points = case_when(plot == 1 ~ 51,
+                                 plot == 2 ~ 57,
+                                 plot == 3 ~ 78,
+                                 plot == 4 ~ 19,
+                                 plot == 5 ~ 72,
+                                 plot == 6 ~ 39,
+                                 plot == 7 ~ 10,
+                                 plot == 8 ~ 21),
+         plot = as.factor(plot),
+         Method = as.factor(Method))
+
+
+# ...................... # occurrence points plot ....................
+# all variable combinations
+# ggplot(results_plot_cor) +
+#   geom_point(aes(x = auc,
+#                  y = cor,
+#                  col = Variables,
+#                  size = no_p_points)) +
+#   scale_color_manual(values = c("all" = "maroon",
+#                                 "reduced" = "dodgerblue",
+#                                 "select" = "yellowgreen")) +
+#   scale_size_continuous(range = c(1,8)) +
+#   facet_wrap(~Method)
+
+# only reduced variables
+results_plot_cor %>% filter(Variables == "reduced") %>% 
+
+ggplot() +
+  geom_point(aes(x = auc,
+                 y = cor,
+                 size = no_p_points),
+             shape = 1) +
+  scale_size_continuous(range = c(1,9)) +
+  theme_bw() +
+  labs(x = "Test AUC", y = "Correlation",
+       size = "No. Occurrences") +
+  facet_wrap(~Method)
+
+
+
